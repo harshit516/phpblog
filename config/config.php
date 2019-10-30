31b626ec27fb46c15b3c47d696439d5f5a32305d
harshit shukla <37479246+harshit516@users.noreply.github.com>
2019-10-30 15:48:21 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,5 @@
 <?php
 
 define('ROOT_URL','https://testdb516.000webhostapp.com/');
-/*
-define('DB_HOST','localhost');
-define('DB_USER','id11356128_root');
-define('DB_PASS','9892665907');
-define('DB_NAME','id11356128_phpblog');
 
-
-*/
 ?>
